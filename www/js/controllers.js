ca1c10b1c069645a98f9724ab74ba7bbb039a59f
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-20 09:06:01 -0300

    Feita a Base do Projeto, com Ionic creator

---
+++ www/js/controllers.js
@@ -0,0 +1,58 @@
+angular.module('app.controllers', [])
+  
+.controller('principalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('escolhaMaterialCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('perguntaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('acertouCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('errouCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('gameOverCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('ganhouCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,57 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('principal', {
+    url: '/principal',
+    templateUrl: 'templates/principal.html',
+    controller: 'principalCtrl'
+  })
+
+  .state('escolhaMaterial', {
+    url: '/escolha',
+    templateUrl: 'templates/escolhaMaterial.html',
+    controller: 'escolhaMaterialCtrl'
+  })
+
+  .state('pergunta', {
+    url: '/pergunta',
+    templateUrl: 'templates/pergunta.html',
+    controller: 'perguntaCtrl'
+  })
+
+  .state('acertou', {
+    url: '/acertou',
+    templateUrl: 'templates/acertou.html',
+    controller: 'acertouCtrl'
+  })
+
+  .state('errou', {
+    url: '/errou',
+    templateUrl: 'templates/errou.html',
+    controller: 'errouCtrl'
+  })
+
+  .state('gameOver', {
+    url: '/gameover',
+    templateUrl: 'templates/gameOver.html',
+    controller: 'gameOverCtrl'
+  })
+
+  .state('ganhou', {
+    url: '/Ganhou',
+    templateUrl: 'templates/ganhou.html',
+    controller: 'ganhouCtrl'
+  })
+
+$urlRouterProvider.otherwise('/principal')
+
+
+});
