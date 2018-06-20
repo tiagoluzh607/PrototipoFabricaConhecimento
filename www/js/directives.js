f94ee475f8c35102e118117e0b8b0c49a9c3706c
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-20 10:29:12 -0300

    Colocando a barra de Vida em uma diretiva

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,19 @@
 angular.module('app.directives', [])
 
-.directive('blankDirective', [function(){
+.directive('barraVida', [function(){ // usar barra-vida
+
+    var ddo = {}; //uma diretiva sempre retorna um ddo
+    
+            ddo.restric = "E"; //Poderemos usar tanto como Atribute: <div meu-painel></div> tanto como Element: <meu-painel></meu-painel>
+            
+            ddo.scope = {
+               numerovida: '@numerovida' // diz podemos inserir o atributo titulo para o painel: <meu-painel titulo="Teste"></meu-painel>, 
+            }
+            
+           ddo.transclude = true; //permite que no html montado insira elementos filhos
+            ddo.templateUrl = 'templates/directives/barra-vida.html'; //Elemento que ele irá renderizar
+    
+            return ddo;
+
 
 }]);
